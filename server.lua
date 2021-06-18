e131cf4bfb6b4df1047d6f90e316e5dbd1a96c00
KoolAiddTV <51595577+KoolAiddTV@users.noreply.github.com>
2021-06-18 15:25:00 -0400

    add weight check

--- server.lua
+++ server.lua
@@ -7,12 +7,16 @@ RegisterServerEvent('supreme_buy:RepairKitBuy')
 AddEventHandler('supreme_buy:RepairKitBuy', function(amount)
 	local xPlayer  = ESX.GetPlayerFromId(source)
 	local price = 1000
-	if xPlayer.getMoney() > price then
-		xPlayer.addInventoryItem('fixkit',1)
-		xPlayer.removeMoney(price)
-		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'You paid ' .. price, })
+	if xPlayer.canCarryItem('fixkit', 1) then
+		if xPlayer.getMoney() > price then
+			xPlayer.addInventoryItem('fixkit',1)
+			xPlayer.removeMoney(price)
+			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'You paid ' .. price, })
+		else
+			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Not enough money', })
+		end
 	else
-		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Not enough money', })
+		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Can not carry', })
 	end
 			
 end)	
@@ -23,12 +27,16 @@ RegisterServerEvent('supreme_buy:BlowtorchBuy')
 AddEventHandler('supreme_buy:BlowtorchBuy', function(amount)
 	local xPlayer  = ESX.GetPlayerFromId(source)
 	local price = 500
-	if xPlayer.getMoney() > price then
-		xPlayer.addInventoryItem('blowtorch',1)
-		xPlayer.removeMoney(price)
-		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'You paid ' .. price, })
+	if xPlayer.canCarryItem('blowtorch', 1) then
+		if xPlayer.getMoney() > price then
+			xPlayer.addInventoryItem('blowtorch',1)
+			xPlayer.removeMoney(price)
+			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'You paid ' .. price, })
+		else
+			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Not enough money', })
+		end
 	else
-		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Not enough money', })
+		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Can not carry', })
 	end
 end)	
 -- --
@@ -38,12 +46,16 @@ RegisterServerEvent('supreme_buy:WaterBuy')
 AddEventHandler('supreme_buy:WaterBuy', function(amount)
 	local xPlayer  = ESX.GetPlayerFromId(source)
 	local price = 35
-	if xPlayer.getMoney() > price then
-		xPlayer.addInventoryItem('bread',1)
-		xPlayer.removeMoney(price)
-		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'You paid ' .. price, })
+	if xPlayer.canCarryItem('bread', 1) then
+		if xPlayer.getMoney() > price then
+			xPlayer.addInventoryItem('bread',1)
+			xPlayer.removeMoney(price)
+			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'You paid ' .. price, })
+		else
+			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Not enough money', })
+		end
 	else
-		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Not enough money', })
+		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Can not carry', })
 	end
 end)	
 -- --
@@ -53,11 +65,15 @@ RegisterServerEvent('supreme_buy:BreadBuy')
 AddEventHandler('supreme_buy:BreadBuy', function(amount)
 	local xPlayer  = ESX.GetPlayerFromId(source)
 	local price = 35
-	if xPlayer.getMoney() > price then
-		xPlayer.removeMoney(price)
-		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'You paid ' .. price, })
+	if xPlayer.canCarryItem('fixkit', 1) then
+		if xPlayer.getMoney() > price then
+			xPlayer.removeMoney(price)
+			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'You paid ' .. price, })
+		else
+			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Not enough money', })
+		end
 	else
-		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Not enough money', })
+		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Can not carry', })
 	end
 end)	
 -- --

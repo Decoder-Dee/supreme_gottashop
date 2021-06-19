1b36b77063cb01272f8f05e5587ba0d172222215
DuncanEll <59232670+Decoder-Dee@users.noreply.github.com>
2021-06-19 18:24:36 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -2,36 +2,14 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
--- Repairkit --
-RegisterServerEvent('supreme_buy:RepairKitBuy')
-AddEventHandler('supreme_buy:RepairKitBuy', function(amount)
+RegisterServerEvent('supreme_buy:SetBuy')
+AddEventHandler('supreme_buy:SetBuy', function(itemName, price)
 	local xPlayer  = ESX.GetPlayerFromId(source)
-	local price = 1000
-	if xPlayer.canCarryItem('fixkit', 1) then
+	if xPlayer.canCarryItem(itemName, 1) then
 		if xPlayer.getMoney() > price then
-			xPlayer.addInventoryItem('fixkit',1)
+			xPlayer.addInventoryItem(itemName,1)
 			xPlayer.removeMoney(price)
-			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'You paid ' .. price, })
-		else
-			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Not enough money', })
-		end
-	else
-		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Can not carry', })
-	end
-			
-end)	
--- --
-
--- Blowtorch --
-RegisterServerEvent('supreme_buy:BlowtorchBuy')
-AddEventHandler('supreme_buy:BlowtorchBuy', function(amount)
-	local xPlayer  = ESX.GetPlayerFromId(source)
-	local price = 500
-	if xPlayer.canCarryItem('blowtorch', 1) then
-		if xPlayer.getMoney() > price then
-			xPlayer.addInventoryItem('blowtorch',1)
-			xPlayer.removeMoney(price)
-			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'You paid ' .. price, })
+			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'You paid ' .. price, })
 		else
 			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Not enough money', })
 		end
@@ -40,42 +18,3 @@ AddEventHandler('supreme_buy:BlowtorchBuy', function(amount)
 	end
 end)	
 -- --
-
--- Water --
-RegisterServerEvent('supreme_buy:WaterBuy')
-AddEventHandler('supreme_buy:WaterBuy', function(amount)
-	local xPlayer  = ESX.GetPlayerFromId(source)
-	local price = 35
-	if xPlayer.canCarryItem('water', 1) then
-		if xPlayer.getMoney() > price then
-			xPlayer.addInventoryItem('water',1)
-			xPlayer.removeMoney(price)
-			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'You paid ' .. price, })
-		else
-			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Not enough money', })
-		end
-	else
-		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Can not carry', })
-	end
-end)	
--- --
-
--- Bread --
-RegisterServerEvent('supreme_buy:BreadBuy')
-AddEventHandler('supreme_buy:BreadBuy', function(amount)
-	local xPlayer  = ESX.GetPlayerFromId(source)
-	local price = 35
-	if xPlayer.canCarryItem('bread', 1) then
-		if xPlayer.getMoney() > price then
-			xPlayer.addInventoryItem('bread',1)
-			xPlayer.removeMoney(price)
-			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'You paid ' .. price, })
-		else
-			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Not enough money', })
-		end
-	else
-		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Can not carry', })
-	end
-end)	
--- --
-
